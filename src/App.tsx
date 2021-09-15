5ebe1de6e8d91f7ae1755e2b0aa5582e1aedbee6
Filip Pietruszyński <filippietruszynski@protonmail.com>
2021-09-15 13:23:15 +0200

    Implement App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import { CssBaseline } from "@material-ui/core";
+
+import NavBar from "./components/Navbar";
+import Posts from "./components/Posts";
+import Post from "./components/Post";
+
+import * as api from "./api/requests";
+import { IPosts } from "./components/types";
 
 const App: React.FC = () => {
-  return <>Quasi Blog</>;
+  const [posts, setPosts] = useState<IPosts>([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await api.getAllPosts();
+        setPosts(data);
+      } catch (error) {
+        toast.error("Unfortunately, we were unable to fetch posts.");
+      }
+    })();
+  }, [setPosts]);
+
+  return (
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route path={`/posts/:postId`} exact>
+            <NavBar posts={posts} />
+            <Post posts={posts} />
+          </Route>
+          <Route path="/posts" exact>
+            <NavBar posts={posts} />
+            <Posts posts={posts} />
+          </Route>
+          <Route path="/*">
+            <Redirect to="/posts" />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+      <CssBaseline />
+      <ToastContainer />
+    </>
+  );
 };
 
 export default App;
+
+// TODO: Solve NavBar multiplication
+// TODO: Limit :postId routes to the number of posts returned from api
+// TODO: Improve readability of styling code
+// TODO: Implement real error handling
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
-import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,5 +11,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-reportWebVitals();

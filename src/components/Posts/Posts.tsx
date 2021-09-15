1736abdc522e88c5bb08638e2278c89255f98e2d
Filip Pietruszyński <filippietruszynski@protonmail.com>
2021-09-15 13:27:18 +0200

    Implement Posts

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core";
 
-const Posts: React.FC = () => {
-  return <>Posts</>;
+import PostPreview from "../PostPreview";
+
+import { IPost, IPosts } from "../types";
+
+const useStyles = makeStyles((theme) => ({
+  layout: {
+    width: "auto",
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+      width: 600,
+      marginLeft: "auto",
+      marginRight: "auto",
+    },
+  },
+}));
+
+interface IProps {
+  posts: IPosts;
+}
+
+const Posts: React.FC<IProps> = ({ posts }) => {
+  const classes = useStyles();
+
+  const postPreviews =
+    posts &&
+    posts.map((post: IPost) => {
+      return <PostPreview post={post} key={post.id} />;
+    });
+
+  return <main className={classes.layout}>{postPreviews}</main>;
 };
 
 export default Posts;
